e40fdadb4127de92d07e27ee00a1e0fbc85b82dc
Andres Dominguez <adominguez@dvconsultores.com>
2021-09-09 23:20:08 -0400

    Estrategy parameters

--- backtradereth2.py
+++ backtradereth2.py
@@ -4,8 +4,8 @@ import numpy as np
 from math import floor
 from termcolor import colored as cl
 import matplotlib.pyplot as plt
-import sqlite3
 from decimal import *
+import sqlite3
 # plt.rcParams['figure.figsize'] = (20, 10)
 # plt.style.use('fivethirtyeight')
 
@@ -14,16 +14,16 @@ from decimal import *
 # Retrieving historical data from database
 def get_historical_data():
     # Conexión a base de datos
-    db_con = sqlite3.connect('mockba.db')
-    query = "select strftime('%d/%m/%Y %H %M %S', timestamp) close_time"
-    query += ' , cast(close as float) as close'
+    db_con = sqlite3.connect('storage/mockba.db')
+    query = "select timestamp close_time"
+    query += ' , cast(close as float) as close, close_time as ct'
     query += '  from historical_ETHUSDT'
     query += "  where timestamp >= '2021-01-01'"
-    query += "  and timestamp <= '2021-08-31'"
-    query += ' order by close_time'
+    # query += "  and timestamp <= '2021-01-02'"
+    query += " order by 1"
     # print(query)
     df = pd.read_sql(query, con=db_con, index_col='close_time')
-    df.to_excel("data.xlsx")
+    # df.to_excel("data.xlsx")
     return df
 
 eth = get_historical_data()
@@ -32,28 +32,35 @@ eth = get_historical_data()
 position = []
 action = [] # Take action
 invest = 400 # Initial value
-qtycoin = [] # Qty buy
+qty = [] # Qty buy
 counterBuy = 0 # Counter how many ops buy
 counterSell = 0 # Counter how manu ops sell
+counterStopLoss = 0 # Counter to stop when to loose
+counterForceSell = 0 # Force sell
+#
 feeBuy = 0.099921 # 0.9%
 feeBuy = round((feeBuy / 100),9) # Binance fee when buy
 #
 feeSell = 0.1 # 1%
 feeSell = round((feeSell / 100),9) # Binance fee sell
 fee = 0
-#
-marginSell = 0.9
+################STRATEGY PARAMS############################
+###########################################################
+marginSell = 15
 marginSell = marginSell / 100 + 1 # Earning from each sell
+timeFrameForceSell = 288 # 24 hour 20*60/5
 #
-marginBuy = 0.2
+#
+marginBuy = 2
 marginBuy = marginBuy / 100 + 1 # Earning from each buy
+timeFrameStopLoss = 12 # 1 hour 1*60/5
+############################################################
+############################################################
 #
-stopLoss = 2
-stopLoss = stopLoss / 100 + 1 # Stop Loss
-#
-nextBuy = 0
+nextOps = []
 sellFlag = 0
 killOps = 0
+sellFlag = 0
 
 print("Backtesting in progress, this take time...")
 # Firstly, we are defining a function named ‘get_macd’ 
@@ -120,24 +127,74 @@ for i in range(len(macd_signal)):
     if macd_signal[i] > 1:
         position.append(0)
         action.append(0)
+        qty.append(0)
+        nextOps.append(0)
     else:
         position.append(1)
         action.append(0)
+        qty.append(0)
+        nextOps.append(0)
 
 for i in range(len(eth['close'])):
+    # First signals based on macd
     if macd_signal[i] == 1 and counterBuy == 0:
         position[i] = 1
         action[i] = 'buy'
-        sell_flag = 1
-    elif macd_signal[i] == 1:
-        position[i] = 1
-        action[i] = 'buy'  
+        counterBuy =  i
+        fee = (invest / eth['close'][i]) * feeBuy
+        qty[i] = (invest / eth['close'][i]) - fee
+        nextOps[i] = eth['close'][i] * marginSell
+        sellFlag = 1
+        counterForceSell = 1
+    # Now implement our margin and sell if true
+    elif eth['close'][i] >= nextOps[i - (i - counterBuy)] and sellFlag == 1:
+         counterSell = i
+         fee = (((invest / eth['close'][i - (i - counterBuy)]) * eth['close'][i])) * feeSell
+         qty[i] = (qty[i - (i - counterBuy)] * eth['close'][i]) - fee # Sell amount
+         action[i] = 'mySell'
+         nextOps[i] = qty[i] / ((qty[i] / eth['close'][i]) * marginBuy) # Next buy
+         sellFlag = 0
+         counterStopLoss = 1
+    # Force sell  24 hour
+    # elif eth['close'][i] >= (nextOps[i - (i - counterBuy)] * marginLossSell) and sellFlag == 1:
+    elif counterForceSell ==  timeFrameForceSell and sellFlag == 1:
+         # print('Counter Force Sell - ' + str(counterForceSell))
+         fee = (((invest / eth['close'][i - (i - counterBuy)]) * eth['close'][i])) * feeSell
+         qty[i] = (qty[i - (i - counterBuy)] * eth['close'][i]) - fee # Sell amount
+         nextOps[i] = qty[i] / ((qty[i] / eth['close'][i]) * marginBuy) # Next buy
+         sellFlag = 0
+         counterStopLoss = 1
+         action[i] = 'forceSell'
+         counterSell = i 
+    # Now find the next value for sell or apply stop loss 
+    elif eth['close'][i] <= nextOps[i - (i - counterSell)] and sellFlag == 0:
+         counterBuy = i
+         fee = (qty[i - (i - counterSell)] / eth['close'][i]) * feeBuy
+         qty[i] = (qty[i - (i - counterSell)] / eth['close'][i]) - fee # Buy amount
+         nextOps[i] = eth['close'][i] * marginSell
+         action[i] = 'myBuy'
+         sellFlag = 1
+         counterForceSell = 1
+    # elif eth['close'][i] <= (nextOps[i - (i - counterSell)] * marginLossBuy) and counterStopLoss ==  24 and sellFlag == 0: 
+    # Stop Loss after 1 hour of inactivity
+    elif counterStopLoss ==  timeFrameStopLoss and sellFlag == 0:   
+         # print(counterStopLoss)     
+         action[i] = 'stopLoss'
+         counterBuy =  i
+         fee = (qty[i - (i - counterSell)] / eth['close'][i]) * feeBuy
+         qty[i] = (qty[i - (i - counterSell)] / eth['close'][i]) - fee # Buy amount
+         nextOps[i] = (eth['close'][i] * marginSell)
+         sellFlag = 1   
+         counterForceSell = 1
     elif macd_signal[i] == -1:
         position[i] = 0
         action[i] = 'sell'
+        counterStopLoss = counterStopLoss +1 
+        counterForceSell = counterForceSell + 1
     else:
         position[i] = position[i-1]
-        action[i] = 'hold'       
+        counterStopLoss = counterStopLoss + 1 
+        counterForceSell = counterForceSell + 1
 
 macd = eth_macd['macd']
 signal = eth_macd['signal']
@@ -145,13 +202,15 @@ close_price = eth['close']
 macd_signal = pd.DataFrame(macd_signal).rename(columns = {0:'macd_signal'}).set_index(eth.index)
 position = pd.DataFrame(position).rename(columns = {0:'macd_position'}).set_index(eth.index)
 action = pd.DataFrame(action).rename(columns = {0:'macd_action'}).set_index(eth.index)
-
-# frames = [close_price, macd, signal, macd_signal, position, action]
-frames = [close_price,  action]
+qty = pd.DataFrame(qty).rename(columns = {0:'qty'}).set_index(eth.index)
+nextOps = pd.DataFrame(nextOps).rename(columns = {0:'nextOps'}).set_index(eth.index)
+# frames = [close_price, macd, signal, macd_signal, position, action, qty, nextOps]
+frames = [close_price,  action, qty, nextOps]
 strategy = pd.concat(frames, join = 'inner', axis = 1)
 
 print("The strategy")
-# strategy = strategy[strategy['macd_action'] != 'hold']
+# strategy = strategy[strategy['macd_action'] != '0'] 
+# print(strategy)
 strategy.to_excel("ETHUSDT-strategy.xlsx")
 
 print("End")
--- getHistorical.py
+++ getHistorical.py
@@ -20,14 +20,14 @@ binance_client = Client(api_key=binance_api_key, api_secret=binance_api_secret)
 
 
 # Database conection
-db_con = sqlite3.connect('mockba.db')
+db_con = sqlite3.connect('storage/mockba.db')
 
 ### FUNCTIONS
 def minutes_of_new_data(symbol, kline_size, data, source):
     if len(data) > 0:
         old = parser.parse(data["timestamp"].iloc[-1])
     elif source == "binance":
-        old = datetime.strptime("01 Jan 2020", '%d %b %Y')
+        old = datetime.strptime("01 Jan 2021", '%d %b %Y')
     if source == "binance":
         new = pd.to_datetime(binance_client.get_klines(
             symbol=symbol, interval=kline_size)[-1][0], unit='ms')
@@ -36,22 +36,26 @@ def minutes_of_new_data(symbol, kline_size, data, source):
 
 def get_all_binance(symbol, kline_size, save=False):
     filename = '%s-%s-data.csv' % (symbol, kline_size)
+    query = 'select cast(timestamp as text) as timestamp, open, high, low, close, volume, close_time, quote_av, trades, tb_base_av, tb_quote_av, ignore from public."historical_ETHUSDT"'
+    #df = pd.read_sql(query, con=db_con)
+    #if len(df['close']) > 0:
     if os.path.isfile(filename):
         data_df = pd.read_csv(filename)
+        #data_df = df
     else:
         data_df = pd.DataFrame()
     oldest_point, newest_point = minutes_of_new_data(
         symbol, kline_size, data_df, source="binance")
     delta_min = (newest_point - oldest_point).total_seconds()/60
     available_data = math.ceil(delta_min/binsizes[kline_size])
-    if oldest_point == datetime.strptime("01 Jan 2020", '%d %b %Y'):
+    if oldest_point == datetime.strptime("01 Jan 2021", '%d %b %Y'):
         print('Downloading all available %s data for %s. Be patient..!' %
               (kline_size, symbol))
     else:
         print('Downloading %d minutes of new data available for %s, i.e. %d instances of %s data.' % (
             delta_min, symbol, available_data, kline_size))
     klines = binance_client.get_historical_klines(symbol, kline_size, oldest_point.strftime(
-        "%d %b %Y %H:%M:%S"), newest_point.strftime("%d %b %Y %H:%M:%S"))
+        "%d %b %Y %H:%M:%S"), newest_point.strftime("%d %b %Y %H:%M:%S"))  
     data = pd.DataFrame(klines, columns=['timestamp', 'open', 'high', 'low', 'close',
                         'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
     data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
