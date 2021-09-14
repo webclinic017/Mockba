18051ee2104214cae1e5e8b65709a3312df1acfc
Andres Dominguez <adominguez@dvconsultores.com>
2021-09-14 17:18:33 -0400

    Ready for production

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,22 +1,40 @@
+from os import defpath
 import telebot
 from telebot import types
-import openbizview
-import postgrescon as pg
-import psycopg2
-import datetime
-from  decimal import *
-import orders
-import time
+import sqlite3
 import pandas as pd
-import sendmail
-import importlib
 
 # Telegram Bot
 API_TOKEN = '1042444870:AAHEuYUbs2YJrGDUEfd1ZjvomJafqCStMKM'
+db_con = sqlite3.connect('storage/mockba.db', check_same_thread=False)
+
+# Def get next ops
+def getUser():
+    df = pd.read_sql('SELECT * FROM t_login',con=db_con)
+    return df
+
+# Def update ops
+def enable_bot():
+    sql = 'update t_signal set status = 1'
+    cur = db_con.cursor()
+    cur.execute(sql)
+    db_con.commit() 
+    db_con.close
+
+# Def update ops
+def disable_bot():
+    sql = 'update t_signal set status = 0'
+    cur = db_con.cursor()
+    cur.execute(sql)
+    db_con.commit()  
+    db_con.close
+ 
 
 bot = telebot.TeleBot(API_TOKEN)
 final_result = []
-gcodrep = ""
+gyear = ""
+gmonth = ""
+user = getUser()
 ########################################################################################################
 ###############################Operaciones para manejar en el Bot#######################################
 ########################################################################################################
@@ -39,199 +57,141 @@ def command_start(m):
     cid = m.chat.id
     nom = m.chat.first_name
     bot.send_message(cid,
-                    "Bienvenido a Mockba " + nom)
+                    "Welcome to Mockba " + str(nom))
     command_help(m)
 
-# Comando listar
-@bot.message_handler(commands=['listar'])
+
+@bot.message_handler(commands=['list'])
 def command_help(m):
     cid = m.chat.id
-    help_text = "Las siguientes opciones son las disponibles."
+    help_text = "Available options."
     markup = types.ReplyKeyboardMarkup()
-    itema = types.KeyboardButton('/EjecutarIA')
-    itemd = types.KeyboardButton('/ActivarAutobot')
-    iteme = types.KeyboardButton('/DesactivarAutobot')
-    itemf = types.KeyboardButton('/listar')
+    itema = types.KeyboardButton('/tradehistory')
+    itemb = types.KeyboardButton('/enablebot')
+    itemc = types.KeyboardButton('/disablebot')
+    itemd = types.KeyboardButton('/list')
     markup.row(itema)
+    markup.row(itemb)
+    markup.row(itemc)
     markup.row(itemd)
-    markup.row(iteme)
-    markup.row(itemf)
     bot.send_message(cid, help_text, reply_markup=markup) 
 
-# Comando listar
-@bot.message_handler(commands=['symbol'])
-def symbol(m):
-    cid = m.chat.id
-    valor = m.text
-    global gcodrep
-    if openbizview.login_pass(valor) == 0:
-        markup = types.ReplyKeyboardMarkup()
-        itemd = types.KeyboardButton('/listar')
-        markup.row(itemd)
-        bot.send_message(cid, "Token inválido", reply_markup=markup)
-    else:
-        help_text = "Seleccione par."
-        markup = types.ReplyKeyboardMarkup()
-        itema = types.KeyboardButton('BTCUSDT')
-        itemb = types.KeyboardButton('ETHUSDT')
-        itemd = types.KeyboardButton('/listar')
-        markup.row(itema)
-        markup.row(itemb)
-        markup.row(itemd)
-        bot.send_message(cid, help_text, reply_markup=markup) 
-        if  gcodrep == "1":
-          bot.register_next_step_handler_by_chat_id(cid, operationsIA)
-        elif gcodrep == "4":  
-          bot.register_next_step_handler_by_chat_id(cid, operationsAATB)
-        else:  
-          bot.register_next_step_handler_by_chat_id(cid, operationsDATB)       
-             
 
-# Comando ejecutar IA
-@bot.message_handler(commands=['EjecutarIA'])
-def ia(m):
-    cid = m.chat.id
-    global gcodrep
-    gcodrep = "1"
-    bot.send_message(cid, "Ingrese  token:")
-    bot.register_next_step_handler_by_chat_id(cid, symbol)
-
-
-# Comando montar orden
-@bot.message_handler(commands=['ActivarAutobot'])
+@bot.message_handler(commands=['enablebot'])
 def actautobot(m):
     cid = m.chat.id
-    global gcodrep
-    gcodrep = "4"
-    bot.send_message(cid, "Ingrese  token:")
-    bot.register_next_step_handler_by_chat_id(cid, symbol)
+    markup = types.ReplyKeyboardMarkup()
+    itema = types.KeyboardButton('/tradehistory')
+    itemb = types.KeyboardButton('/enablebot')
+    itemc = types.KeyboardButton('/disablebot')
+    itemd = types.KeyboardButton('/list')
+    markup.row(itema)
+    markup.row(itemb)
+    markup.row(itemc)
+    markup.row(itemd)
+    if int(user['token'].values) == cid:
+        enable_bot()
+        bot.send_message(cid, "Bot enabled....", parse_mode='Markdown', reply_markup=markup)
+    else:    
+        bot.send_message(cid, "User not autorized", parse_mode='Markdown', reply_markup=markup)
 
-# Comando montar orden
-@bot.message_handler(commands=['DesactivarAutobot'])
+
+@bot.message_handler(commands=['disablebot'])
 def dautobot(m):
     cid = m.chat.id
+    markup = types.ReplyKeyboardMarkup()
+    itema = types.KeyboardButton('/tradehistory')
+    itemb = types.KeyboardButton('/enablebot')
+    itemc = types.KeyboardButton('/disablebot')
+    itemd = types.KeyboardButton('/list')
+    markup.row(itema)
+    markup.row(itemb)
+    markup.row(itemc)
+    markup.row(itemd)
+    if  int(user['token'].values) == cid:
+        disable_bot()
+        bot.send_message(cid, "Bot disabled....", parse_mode='Markdown', reply_markup=markup)
+    else:    
+        bot.send_message(cid, "User not autorized", parse_mode='Markdown', reply_markup=markup) 
+
+@bot.message_handler(commands=['tradehistory'])
+def years(m):
+    cid = m.chat.id
+    markup = types.ReplyKeyboardMarkup()
+    item1 = types.KeyboardButton('2021')
+    item2 = types.KeyboardButton('2022')
+    item3 = types.KeyboardButton('2023')
+    item4 = types.KeyboardButton('2024')
+    item5 = types.KeyboardButton('2025')
+    itemb = types.KeyboardButton('/list')
+    markup.row(item1)
+    markup.row(item2)
+    markup.row(item3)
+    markup.row(item4)
+    markup.row(item5)
+    markup.row(itemb)
+    bot.send_message(cid, "Select year:", parse_mode='Markdown', reply_markup=markup)
+    bot.register_next_step_handler_by_chat_id(cid, months)
+
+def months(m):
+    cid = m.chat.id
+    valor = m.text
+    global gyear
+    gyear = valor
+    markup = types.ReplyKeyboardMarkup()
+    item1 = types.KeyboardButton('01')
+    item2 = types.KeyboardButton('02')
+    item3 = types.KeyboardButton('03')
+    item4 = types.KeyboardButton('04')
+    item5 = types.KeyboardButton('05')
+    item6 = types.KeyboardButton('06')
+    item7 = types.KeyboardButton('07')
+    item8 = types.KeyboardButton('08')
+    item9 = types.KeyboardButton('09')
+    item10 = types.KeyboardButton('10')
+    item11 = types.KeyboardButton('11')
+    item12 = types.KeyboardButton('12')
+    itemb = types.KeyboardButton('/list')
+    markup.row(item1, item2, item3, item4)
+    markup.row(item5, item6, item7, item8)
+    markup.row(item9, item10, item11, item12)
+    markup.row(itemb)
+    bot.send_message(cid, "Select month:", parse_mode='Markdown', reply_markup=markup)
+    bot.register_next_step_handler_by_chat_id(cid, trade)                
+
+def trade(m):
+    cid = m.chat.id
+    global gyear
+    markup = types.ReplyKeyboardMarkup()
+    itema = types.KeyboardButton('/tradehistory')
+    itemb = types.KeyboardButton('/enablebot')
+    itemc = types.KeyboardButton('/disablebot')
+    itemd = types.KeyboardButton('/list')
+    markup.row(itema)
+    markup.row(itemb)
+    markup.row(itemc)
+    markup.row(itemd)
+    if  int(user['token'].values) == cid:
+        query  = "select qty, nextOps, ops, STRFTIME('%d/%m/%Y %H:%M',datetime(close_time/1000, 'unixepoch')) close_time from trader_history"
+        query += " where STRFTIME('%Y',datetime(close_time/1000, 'unixepoch')) = '" + gyear + "'"
+        query += " and STRFTIME('%m',datetime(close_time/1000, 'unixepoch')) = '" + m.text + "'"
+        df = pd.read_sql(query,con=db_con)
+        bot.send_message(cid, 'Tranding history')
+        for i in df.index:
+            bot.send_message(cid, 'Close time: ' +  str(df['close_time'][i]) + "\n Next Ops: " + str(df['nextOps'][i]) + " \n Op: " + str(df['ops'][i]) + " \n Qty: " + str(df['qty'][i]), parse_mode='Markdown', reply_markup=markup)
+    else:    
+        bot.send_message(cid, "User not autorized", parse_mode='Markdown', reply_markup=markup) 
+
+
+# default handler for every other text
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def command_default(m):
+    # this is the standard reply to a normal message
     global gcodrep
-    gcodrep = "5"
-    bot.send_message(cid, "Ingrese  token:")
-    bot.register_next_step_handler_by_chat_id(cid, symbol)            
-
-# Calcular IA
-def operationsIA(m):
-    cid = m.chat.id
-    valor = m.text
-    global final_result
-    if valor == "BTCUSDT":
-        markup = types.ReplyKeyboardMarkup()
-        itemd = types.KeyboardButton('/listar')
-        markup.row(itemd)  
-        
-        bot.send_message(cid, "Operación de IA BTCUSDT en ejecución, espere el resultado, puede tardar un tiempo, ejecutando......")  
-        # Ejecita Ia importando objeto
-        # import analizeHistorical
-        # analizeHistorical.analize()  
-        # Al culminar imprimir valores
-        con = pg.conection()
-        try:
-         cur = con.cursor()
-         query = "select cast(cast(close_time as timestamp) at time zone 'utc' at time zone 'america/caracas' as text) as close_time, close_pred from predict_btc_udst"
-         cur.execute(query)       
-         final_result = cur.fetchall()
-         cur.close
-         con.close    
-        except (Exception, psycopg2.DatabaseError) as error:
-          print(error)
-        finally:
-          if con is not None:
-            con.close()  # Cerrando conección  
-        
-        bot.send_message(cid, "Predicción próximos 10 registros de 5 minutos fecha y monto: \n" 
-           , parse_mode='Markdown')  
-
-        for row in final_result:
-           fecha = row[0]
-           monto = round(Decimal(row[1]),2)
-
-           bot.send_message(cid, fecha + '  ---  ' + str(monto), parse_mode='Markdown')   
-
-        #img = open('BTC_validation.png', 'rb')
-        #bot.send_photo(cid, img, caption = "Predicción vs actual")
-
-        #img = open('BTC_predictions.png', 'rb')
-        #bot.send_photo(cid, img, caption = "Predicción 10 períodos", reply_markup=markup)
-        bot.send_message(cid, "Predicción 10 períodos", parse_mode='Markdown')
-    else:
-        markup = types.ReplyKeyboardMarkup()
-        itemd = types.KeyboardButton('/listar')
-        markup.row(itemd)  
-
-        bot.send_message(cid, "Operación de IA ETHUSDT en ejecución, espere el resultado, puede tardar un tiempo, ejecutando......")  
-        # Ejecita Ia importando objeto
-        # import analizeHistoricalEth
-        # analizeHistoricalEth.analize()  
-        # Al culminar imprimir valores
-        con = pg.conection()
-        try:
-         cur = con.cursor()
-         query = "select cast(cast(close_time as timestamp) at time zone 'utc' at time zone 'america/caracas' as text) as close_time, close_pred from predict_eth_udst"
-         cur.execute(query)       
-         final_result = cur.fetchall()
-         cur.close
-         con.close    
-        except (Exception, psycopg2.DatabaseError) as error:
-          print(error)
-        finally:
-          if con is not None:
-            con.close()  # Cerrando conección  
-        
-        bot.send_message(cid, "Predicción próximos 10 registros de 5 minutos fecha y monto: \n" 
-           , parse_mode='Markdown')  
-
-        for row in final_result:
-           fecha = row[0]
-           monto = round(Decimal(row[1]),2)
-
-           bot.send_message(cid, fecha + '  ---  ' + str(monto), parse_mode='Markdown')   
-
-        #img = open('Eth_validation.png', 'rb')
-        #bot.send_photo(cid, img, caption = "Predicción vs actual")
-
-        #img = open('Eth_predictions.png', 'rb')
-        #bot.send_photo(cid, img, caption = "Predicción 10 períodos", reply_markup=markup) 
-        bot.send_message(cid, "Predicción 10 períodos", parse_mode='Markdown')       
-
-
-# Activar bot
-def operationsAATB(m):
-    cid = m.chat.id
-    valor = m.text
-    if valor == "BTCUSDT":
-        markup = types.ReplyKeyboardMarkup()
-        itemd = types.KeyboardButton('/listar')
-        markup.row(itemd)
-        pg.enable_autobot('1')
-        bot.send_message(cid, "Bot BTCUSDT activado", parse_mode='Markdown', reply_markup=markup) 
-    else:
-        markup = types.ReplyKeyboardMarkup()
-        itemd = types.KeyboardButton('/listar')
-        markup.row(itemd)
-        pg.enable_autobot('2')
-        bot.send_message(cid, "Bot ETHUSDT activado", parse_mode='Markdown', reply_markup=markup) 
-
-# Desactivar bot
-def operationsDATB(m):
-    cid = m.chat.id
-    valor = m.text
-    if valor == "BTCUSDT":
-        markup = types.ReplyKeyboardMarkup()
-        itemd = types.KeyboardButton('/listar')
-        markup.row(itemd)
-        pg.disable_autobot('1')
-        bot.send_message(cid, "Bot BTCUSDT desactivado", parse_mode='Markdown', reply_markup=markup) 
-    else:
-        markup = types.ReplyKeyboardMarkup()
-        itemd = types.KeyboardButton('/listar')
-        markup.row(itemd)
-        pg.disable_autobot('2')
-        bot.send_message(cid, "Bot ETHUSDT desactivado", parse_mode='Markdown', reply_markup=markup) 
+    gcodrep = ""
+    markup = types.ReplyKeyboardMarkup()
+    iteme = types.KeyboardButton('/list')
+    markup.row(iteme)
+    bot.send_message(m.chat.id, "Does not understand \"" + m.text + "\"\n , try with \n \n /list" , reply_markup=markup)        
 
 bot.polling()     
--- tradereth.py
+++ tradereth.py
@@ -4,24 +4,27 @@ import sqlite3
 import pandas as pd
 import requests
 import time
+import sendmail as sm
 
 api_key = api.Api().api_key
 api_secret = api.Api().api_secret
 client = Client(api_key, api_secret)
-db_con = sqlite3.connect('storage/mockba.db')
+db_con = sqlite3.connect('storage/mockba.db', check_same_thread=False)
 
 print('Trading')
 # Variables for trading
-invest = 400 # Initial value
+invest = float(client.get_asset_balance(asset='USDT')['free']) #400 # Initial value
+balance_usdt = float(client.get_asset_balance(asset='USDT')['free'])
+balance_eth = float(client.get_asset_balance(asset='ETH')['free'])
 qty = 0 # Qty buy
 counterStopLoss = 0 # Counter to stop when to loose
 counterForceSell = 0 # Force sell
 #
 feeBuy = 0.099921 # 0.9%
-feeBuy = round((feeBuy / 100),9) # Binance fee when buy
+feeBuy = (feeBuy / 100) # Binance fee when buy
 #
 feeSell = 0.1 # 1%
-feeSell = round((feeSell / 100),9) # Binance fee sell
+feeSell = (feeSell / 100) # Binance fee sell
 fee = 0
 ################STRATEGY PARAMS############################
 ###########################################################
@@ -38,7 +41,6 @@ timeFrameStopLoss = 288 # 24 hour 24*60/5
 #
 nextOps = 0
 sellFlag = 0
-# asset_balance_coin = client.get_asset_balance(asset='ETH')['free']
 #
 
 #
@@ -74,6 +76,7 @@ def update_trader_close_time(close_time):
 def getNextOps():
     df = pd.read_sql('SELECT * FROM trader',con=db_con)
     return df
+    
 
 while True:
     # get ticker
@@ -90,57 +93,73 @@ while True:
         if operations['counterBuy'].values == 0:
             print('First Buy')
             fee = (invest / float(eth[4][499])) * feeBuy
-            qty = (invest / float(eth[4][499])) - fee
+            qty = ((invest / float(eth[4][499])) - fee) - 0.0001
             nextOps = float(eth[4][499]) * marginSell
             sellFlag = 1
             counterForceSell = 1
             data = (qty,nextOps,sellFlag,counterStopLoss,counterForceSell,1,'buy',str(eth[0][499]))
             act_trader_nextOps(data)
-            # client.order_market_buy(symbol="ETHUSDT", quantity=qty)
+            client.order_market_buy(symbol="ETHUSDT", quantity=round(qty,4))
+            sm.sendMail('First Buy')
+            time.sleep(3)
+            print('Done...')
         # Sell    
         elif float(eth[4][499]) >= operations['nextOps'].values and operations['sellFlag'].values == 1:
             print('Sell')
-            fee = (operations['qty'].values * float(eth[4][499]) * feeSell)
-            qty = (operations['qty'].values * float(eth[4][499]) - fee) # Sell amount
+            fee = (balance_eth * feeSell)
+            qty = (balance_eth  - fee) - 0.0001 # Sell amount
             nextOps = qty / ((qty / float(eth[4][499]) * marginBuy)) # Next buy
             counterStopLoss = 1
             data = (float(qty),float(nextOps),sellFlag,counterStopLoss,counterForceSell,1,'sell',str(eth[0][499]))
             act_trader_nextOps(data)
-            # client.order_market_sell(symbol="ETHUSDT", quantity=qty)
+            client.order_market_sell(symbol="ETHUSDT", quantity=round(qty,4))
+            sm.sendMail('Sell')
+            time.sleep(3)
+            print('Done...')
         # force sell     
         elif operations['counterForceSell'].values ==  timeFrameForceSell and operations['sellFlag'].values == 1:
             print('Force Sell')
-            fee = (operations['qty'].values * float(eth[4][499]) * feeSell)
-            qty = (operations['qty'].values * float(eth[4][499]) - fee) # Sell amount
+            fee = (balance_eth * feeSell)
+            qty = (balance_eth  - fee) - 0.0001 # Sell amount
             nextOps = qty / ((qty / float(eth[4][499]) * marginBuy)) # Next buy
+            print(round(qty,4))
             counterStopLoss = 1 
             data = (float(qty),float(nextOps),sellFlag,counterStopLoss,counterForceSell,1,'forceSell',str(eth[0][499]))
             act_trader_nextOps(data)
-            # client.order_market_sell(symbol="ETHUSDT", quantity=qty) 
+            client.order_market_sell(symbol="ETHUSDT", quantity=round(qty,4))
+            sm.sendMail('Force Sell')
+            time.sleep(3)
+            print('Done...')
         # Buy     
         elif float(eth[4][499]) <= operations['nextOps'].values and operations['sellFlag'].values == 0:
             print('Buy')
-            fee = (operations['qty'].values / float(eth[4][499])) * feeBuy
-            qty = (operations['qty'].values / float(eth[4][499])) - fee # Buy amount
+            fee = (balance_usdt / float(eth[4][499])) * feeBuy
+            qty = (balance_usdt / float(eth[4][499]) - fee) - 0.0001 # Buy amount
             nextOps = float(eth[4][499]) * marginSell
             sellFlag = 1
             counterForceSell = 1 
             data = (float(qty),float(nextOps),sellFlag,counterStopLoss,counterForceSell,1,'buy',str(eth[0][499]))
             act_trader_nextOps(data)
-            # client.order_market_buy(symbol="ETHUSDT", quantity=qty)
+            client.order_market_buy(symbol="ETHUSDT", quantity=round(qty,4))
+            sm.sendMail('Buy')
+            time.sleep(3)
+            print('Done...')
         elif operations['counterStopLoss'].values ==  timeFrameStopLoss and operations['sellFlag'].values == 0:   
             print('Stop Loss')     
-            fee = (operations['qty'].values / float(eth[4][499])) * feeBuy
-            qty = (operations['qty'].values / float(eth[4][499])) - fee # Buy amount
+            fee = (balance_usdt / float(eth[4][499])) * feeBuy
+            qty = (balance_usdt / float(eth[4][499]) - fee) - 0.0001 # Buy amount
             nextOps = float(eth[4][499]) * marginSell
             sellFlag = 1
             counterForceSell = 1 
             data = (float(qty),float(nextOps),sellFlag,counterStopLoss,counterForceSell,1,'stopLoss',str(eth[0][499]))
             act_trader_nextOps(data)  
-            # client.order_market_buy(symbol="ETHUSDT", quantity=qty)    
+            client.order_market_buy(symbol="ETHUSDT", quantity=round(qty,4))
+            sm.sendMail('Stop Loss')
+            time.sleep(3)
+            print('Done...')   
         else:
             update_trader_nextOps()
             update_trader_close_time(eth[0][499])
     else:
         print('Waiting candelstick 5m close...........') 
-    time.sleep(20)    
+    time.sleep(30)    
